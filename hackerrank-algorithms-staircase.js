d598eaba09d1a31c6c08bcd71d6c529fbe1f1355
Pablo Rebolini <pablo.rebolini@gruports.com>
2020-03-11 12:59:26 +0100

    HackerRank - Staircase

--- hackerrank-algorithms-staircase.js
+++ hackerrank-algorithms-staircase.js
@@ -1,5 +1,8 @@
 /**
  * https://www.hackerrank.com/challenges/staircase/problem
+ *
+ * String.repeat() efficiency
+ *   https://2ality.com/2014/01/efficient-string-repeat.html
  */
 let i = 0;
 let n = 6;
